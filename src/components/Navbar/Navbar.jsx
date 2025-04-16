e979a7c24b53a5c47f96caabad37c96dfecbd483
Ricardo Figueiredo <ricardocastrofigueiredo@gmail.com>
2025-04-16 23:20:20 +0100

    add Navbar component with Menubar from PrimeReact

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,44 @@
 import { Link } from "react-router-dom";
 import ThemeToggle from "../ThemeToggle/ThemeToggle.jsx";
+import { Menubar } from "primereact/menubar";
+import styles from "./Navbar.module.css";
 
 function Navbar() {
+  const logo = <img src="/icons/owl.svg" alt="logo" className={styles.logo} />;
+  const items = [
+    {
+      label: "Logo",
+      template: () => <Link to="/">{logo}</Link>,
+    },
+    {
+      label: "Projects",
+      template: () => <Link to="/projects">Projects</Link>,
+    },
+    {
+      label: "Education",
+      template: () => <Link to="/education">Education</Link>,
+    },
+    {
+      label: "Skills",
+      template: () => <Link to="/skills">Skills</Link>,
+    },
+    {
+      label: "Experience",
+      template: () => <Link to="/experience">Experience</Link>,
+    },
+    {
+      label: "Contact",
+      template: () => <a href="#contact">Contact</a>,
+    },
+    {
+      label: "ThemeToggle",
+      template: () => <ThemeToggle />,
+    },
+  ];
+
   return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/education">Education</Link>
-        </li>
-        <li>
-          <Link to="/skills">Skills</Link>
-        </li>
-        <li>
-          <Link to="/experience">Experience</Link>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-        <li>
-          <ThemeToggle />
-        </li>
-      </ul>
+    <nav>
+      <Menubar model={items} className={styles.menubar} />
     </nav>
   );
 }
