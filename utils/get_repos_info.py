9415e6d7bc507c44866c28fd38601bcd3964d248
Ricardo Figueiredo <ricardocastrofigueiredo@gmail.com>
2025-04-18 19:34:07 +0100

    refactor get_repos_info.py to improve data fetching and structure JSON output

--- utils/get_repos_info.py
+++ utils/get_repos_info.py
@@ -1,30 +1,85 @@
 import os
 import requests
 import base64
+import json
+from dotenv import load_dotenv
 
-GITHUB_USERNAME = "ricardofig016"
+
+load_dotenv()
+
 REPO_CODES = ["kotlin-compiler", "shinsu-duel", "feedback-circle"]
+
 REPOS_DIR_PATH = "public/data/repos"
 
+GITHUB_USERNAME = "ricardofig016"
+GITHUB_API_BASE_URL = f"https://api.github.com/repos/{GITHUB_USERNAME}"
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
-def get_repo_readme(repo_code):
-    url = f"https://api.github.com/repos/{GITHUB_USERNAME}/{repo_code}/readme"
-    response = requests.get(url)
 
+def get_request(url):
+    headers = {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": f"token {GITHUB_TOKEN}",
+        "User-Agent": GITHUB_USERNAME,
+    }
+    response = requests.get(url, headers=headers)
     if response.status_code != 200:
-        print(f"Failed to fetch README for {repo_code}")
-        return
+        print(f"Failed to fetch data: {response.status_code} - {response.text}")
+        return None
+    return response.json()
 
-    data = response.json()
+
+def get_general_info(repo_code):
+    url = f"{GITHUB_API_BASE_URL}/{repo_code}"
+    data = get_request(url)
+    if data is None:
+        return None
+    repo_info = {
+        "description": data["description"],
+        "stars": data["stargazers_count"],
+        "forks": data["forks_count"],
+        "issues": data["open_issues_count"],
+        "url": data["html_url"],
+    }
+    return repo_info
+
+
+def get_languages(repo_code):
+    url = f"{GITHUB_API_BASE_URL}/{repo_code}/languages"
+    return get_request(url)
+
+
+def get_readme(repo_code):
+    url = f"{GITHUB_API_BASE_URL}/{repo_code}/readme"
+    data = get_request(url)
+    if data is None:
+        return None
     readme = base64.b64decode(data["content"]).decode("utf-8")
+    return readme
 
-    repo_dir_path = os.path.join(REPOS_DIR_PATH, repo_code)
-    os.makedirs(repo_dir_path, exist_ok=True)
-    with open(os.path.join(repo_dir_path, "README.md"), "w", encoding="utf-8") as file:
-        file.write(readme)
+
+def get_name(readme):
+    for line in readme.splitlines():
+        line = line.strip()
+        if line.startswith("# "):
+            return line[2:].strip()
+    return "Untitled"
 
 
 if __name__ == "__main__":
     os.makedirs(REPOS_DIR_PATH, exist_ok=True)
     for repo_code in REPO_CODES:
-        get_repo_readme(repo_code)
+        print(f"Fetching data for {repo_code}...")
+        repo_data = {}
+        repo_data["code"] = repo_code
+        general_info = get_general_info(repo_code)
+        for key, value in general_info.items():
+            repo_data[key] = value
+        repo_data["languages"] = get_languages(repo_code)
+        repo_data["readme"] = get_readme(repo_code)
+        repo_data["name"] = get_name(repo_data["readme"])
+
+        repo_file_name = f"{repo_code}.json"
+        repo_file_path = os.path.join(REPOS_DIR_PATH, repo_file_name)
+        with open(repo_file_path, "w", encoding="utf-8") as file:
+            json.dump(repo_data, file, indent=4, sort_keys=True)
