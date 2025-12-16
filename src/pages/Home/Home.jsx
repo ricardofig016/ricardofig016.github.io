33463ddaa6f1cbd2a629cf99e4fce1cd3ae2aea6
Ricardo Figueiredo <ricardocastrofigueiredo@gmail.com>
2025-12-16 19:40:36 +0000

    feat: add minimalist scrollbar styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -62,7 +62,7 @@ function Home({ featuredProjectsData }) {
         <div className={styles.heroText}>
           <h1>Ricardo Figueiredo</h1>
           <div>
-            <p>Backend Developer</p>
+            <p>Fullstack Developer</p>
             <p>Building Software & Learning Every Day</p>
           </div>
           <div className={styles.heroActions}>
